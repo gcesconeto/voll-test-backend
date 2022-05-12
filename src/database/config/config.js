198cb0ee81f23db6f6dcc23797212068f68b0e7f
Guilherme Cesconeto <cesconeto@gmail.com>
2022-05-12 14:35:51 -0300

    fix: trying to connect without ssl

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DB_NAME,
-    host: process.env.PG_HOST,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+    // ssl: {
+    //    require: false,
+    //    rejectUnauthorized: false
+    //  }
+   }
   },
   test: {
     username: process.env.PG_USER,
@@ -18,11 +21,11 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    dialectOptions: {
-    ssl: {
-       require: true,
-       rejectUnauthorized: false
-     }
-   }
+  //   dialectOptions: {
+  //   // ssl: {
+  //   //    require: true,
+  //   //    rejectUnauthorized: false
+  //   //  }
+  //  }
   },
 };
